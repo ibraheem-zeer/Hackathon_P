6e78b12d10c9928ed5cd013b2155382c99361b96
ibraheem Zeer <211400@ppu.edu.ps>
2023-09-02 12:16:30 +0300

    start edits

---
+++ sample.ts
@@ -0,0 +1,11 @@
+// import { accessKeyIdType } from './node_modules/aws-sdk/clients/iam.d';
+// import { region } from './node_modules/aws-sdk/clients/health.d';
+// const AWS = require('aws-sdk')
+
+// let s3 = new AWS.s3({
+//     region: 'eu-west-2',
+//     accessKeyId: 'AKIA3SQWPZW4SHMS2HH5',
+
+// })
+
+// s3.createbu
