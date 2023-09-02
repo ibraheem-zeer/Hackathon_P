97d04f45e71004f31c4df4cb55047f759c72c731
ibraheem Zeer <211400@ppu.edu.ps>
2023-09-02 14:02:11 +0300

    4

--- app.ts
+++ app.ts
@@ -1,31 +1,126 @@
+import express from 'express';
+import multer from 'multer';
 import AWS from 'aws-sdk';
-import fs from 'fs';
+import fs from 'fs'
 
-// Initialize the AWS SDK
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Configure AWS with your credentials
 AWS.config.update({
-    region: 'eu-west-2',
-    accessKeyId: 'AKIA3SQWPZW4WIK5OJNX',
-    secretAccessKey: 'uQFFKTDs9qe/VROS/zMZjEbM3jkqGk1hDV9+hd+H'
+    region: 'eu-west-2', // Replace with your AWS region
+    accessKeyId: 'AKIA3SQWPZW4WIK5OJNX', // Replace with your AWS access key ID
+    secretAccessKey: 'uQFFKTDs9qe/VROS/zMZjEbM3jkqGk1hDV9+hd+H', // Replace with your AWS secret access key
 });
 
-// Use Rekognition
 const rekognition = new AWS.Rekognition();
 
-// Load local image
-const filePath = 'main.jpg';  // Adjust this to your image's path
-const imageBytes = fs.readFileSync(filePath);
-
-const params = {
-    Image: {
-        Bytes: imageBytes
+// Configure Multer for handling file uploads
+const storage = multer.diskStorage({
+    destination: (req, file, callback) => {
+        callback(null, 'uploads/');
     },
-    Attributes: ['ALL']
-};
-
-rekognition.detectFaces(params, function (err: any, data: any) {
-    if (err) {
-        console.error("Error:", err, err.stack);
-    } else {
-        console.log("Rekognition Result:", data);
+    filename: (req, file, callback) => {
+        callback(null, Date.now() + '-' + file.originalname)
     }
+});
+
+const upload = multer({ storage });
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Define a route for uploading and analyzing an image
+app.post('/analyze', upload.single('file'), (req, res) => {
+    if (!req.file) {
+        res.status(500).send("Failed Upload File!");
+        return;
+    }
+
+    // Load the image file from disk
+    const fileBuffer = fs.readFileSync(req.file.path);
+
+    // Configure parameters for Rekognition
+    const params = {
+        Image: {
+            Bytes: fileBuffer,
+        },
+    };
+
+    // Use Rekognition to detect labels in the image
+    rekognition.detectLabels(params, (err, data) => {
+        if (err) {
+            console.error('Error analyzing image:', err);
+            res.status(500).json({ error: 'Failed to analyze image' });
+        } else {
+            const labels = data.Labels?.map((label) => label.Name);
+            res.json({ labels });
+        }
+    });
+});
+
+
+app.post('/spot-celebrities', upload.single('file'), (req, res) => {
+    if (!req.file) {
+        res.status(500).send("Failed Upload File!");
+        return;
+    }
+
+    // Load the image file from disk
+    const fileBuffer = fs.readFileSync(req.file.path);
+
+    // Configure parameters for Rekognition to recognize celebrities
+    const params = {
+        Image: {
+            Bytes: fileBuffer,
+        },
+    };
+
+    // Use Rekognition to recognize celebrities in the image
+    rekognition.recognizeCelebrities(params, (err, data) => {
+        if (err) {
+            console.error('Error spotting celebrities:', err);
+            res.status(500).json({ error: 'Failed to spot celebrities' });
+        } else {
+            const celebrities = data.CelebrityFaces?.map((celeb) => celeb.Name);
+            res.json({ celebrities });
+        }
+    });
+});
+
+
+app.post('/extract-text', upload.single('file'), (req, res) => {
+    if (!req.file) {
+        res.status(500).send("Failed Upload File!");
+        return;
+    }
+
+    // Load the image file from disk
+    const fileBuffer = fs.readFileSync(req.file.path);
+
+    // Configure parameters for Rekognition to detect text
+    const params = {
+        Image: {
+            Bytes: fileBuffer,
+        },
+    };
+
+    // Use Rekognition to detect text in the image
+    rekognition.detectText(params, (err, data) => {
+        if (err) {
+            console.error('Error extracting text:', err);
+            res.status(500).json({ error: 'Failed to extract text' });
+        } else {
+            const extractedText = data.TextDetections?.map((text) => text.DetectedText);
+            res.json({ extractedText });
+        }
+    });
+});
+
+
+// Add other routes for file retrieval or additional functionality as needed
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
--- sample.ts
+++ sample.ts
@@ -1,31 +1,31 @@
-const AWS = require('aws-sdk');
-const fs = require('fs');
+// import AWS from 'aws-sdk';
+// import fs from 'fs';
 
-// Initialize the AWS SDK
-AWS.config.update({
-    region: 'eu-west-2',
-    accessKeyId: 'AKIA3SQWPZW4WIK5OJNX',
-    secretAccessKey: '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'
-});
+// // Initialize the AWS SDK
+// AWS.config.update({
+//     region: 'eu-west-2',
+//     accessKeyId: 'AKIA3SQWPZW4WIK5OJNX',
+//     secretAccessKey: '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'
+// });
 
-// Use Rekognition
-const rekognition = new AWS.Rekognition();
+// // Use Rekognition
+// const rekognition = new AWS.Rekognition();
 
-// Load local image
-const filePath = 'main.jpg';  // Adjust this to your image's path
-const imageBytes = fs.readFileSync(filePath);
+// // Load local image
+// const filePath = 'main.jpg';  // Adjust this to your image's path
+// const imageBytes = fs.readFileSync(filePath);
 
-const params = {
-    Image: {
-        Bytes: imageBytes
-    },
-    Attributes: ['ALL']
-};
+// const params = {
+//     Image: {
+//         Bytes: imageBytes
+//     },
+//     Attributes: ['ALL']
+// };
 
-rekognition.detectFaces(params, function (err: any, data: any) {
-    if (err) {
-        console.error("Error:", err, err.stack);
-    } else {
-        console.log("Rekognition Result:", data);
-    }
-});
+// rekognition.detectFaces(params, function (err: any, data: any) {
+//     if (err) {
+//         console.error("Error:", err, err.stack);
+//     } else {
+//         console.log("Rekognition Result:", data);
+//     }
+// });
